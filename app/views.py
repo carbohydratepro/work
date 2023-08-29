3a6a01a733fd019ce434ee2bcaf609500665c1b6
carbohydratepro <carbohydratepro@gmail.com>
2023-08-30 06:03:10 +0900

    taskモデルを修正し、名前をshiftに変更

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.template import loader
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Shift
+import json
 
 
 def display_calendar(request):
@@ -9,7 +11,19 @@ def display_calendar(request):
     return HttpResponse(template.render())
 
 def detail(request, date):
-    return render(request, 'app/detail.html', {'date': date})
+    shifts = Shift.objects.all()
+    data = [{
+        'shift': shift.name,
+        'Start': shift.start_time.strftime('%Y-%m-%d %H:%M'),
+        'Finish': shift.end_time.strftime('%Y-%m-%d %H:%M'),
+        'Resource': 'Shift'
+    } for shift in shifts]
+    
+    context = {
+        'data': json.dumps(data),
+        'date': date,
+    }
+    return render(request, 'app/detail.html', context)
 
 def new(request):
     return render(request, 'app/new.html')
