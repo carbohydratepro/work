e9ed90404eacc59ff3d4b6fe666c129df4680595
carbohydratepro <carbohydratepro@gmail.com>
2023-09-02 03:37:38 +0900

    投稿機能の修正

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,15 @@ from django import forms
 from .models import Shift
 
 class ShiftForm(forms.ModelForm):
+    start_time = forms.DateTimeField(
+        widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        input_formats=['%Y-%m-%dT%H:%M']
+    )
+    end_time = forms.DateTimeField(
+        widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        input_formats=['%Y-%m-%dT%H:%M']
+    )
+    
     class Meta:
         model = Shift
         fields = ['applicant_name', 'substitute_name', 'start_time', 'end_time', 'is_substitute_found']
--- project/settings.py
+++ project/settings.py
@@ -134,3 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'  # 例として、ホームページにリダイレクトする場合
 LOGIN_URL = '/login/'
+
+# ALLOWED_HOSTS = ['*']
