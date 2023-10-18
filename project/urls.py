9b7f465ff613c785b355061147bd32d69b0f84a0
carbohydratepro <carbohydratepro@gmail.com>
2023-10-18 10:48:07 +0900

    ログインしていないユーザーのアクセス制限を追加

--- project/urls.py
+++ project/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('admin/', admin.site.urls), # この行をコメントアウト
